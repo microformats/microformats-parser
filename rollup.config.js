aaa554a0c0315a661c3355ec7508408c5a086c79
Aimee Gamble-Milner <12508200+aimee-gm@users.noreply.github.com>
2022-06-02 19:48:42 +0100

    Fix rollup warning

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import sourceMaps from "rollup-plugin-sourcemaps";
 import { terser } from "rollup-plugin-terser";
-import html from "@web/rollup-plugin-html";
-import serve from "rollup-plugin-serve";
 
 export default [
   {
     input: "./src/index.ts",
-    // \0 is rollup convention for generated in memory modules
-    external: (id) =>
-      !id.startsWith("\0") && !id.startsWith(".") && !id.startsWith("/"),
     plugins: [
       typescript({
         outputToFilesystem: true,
