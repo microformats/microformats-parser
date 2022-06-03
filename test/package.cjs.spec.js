8a7a9f93d95dc40b04a5c1ff8ca90db2ffd32a27
Aimee Gamble-Milner <12508200+aimee-gm@users.noreply.github.com>
2022-06-03 15:34:21 +0100

    bump node version; drop support for node <14

---
+++ test/package.cjs.spec.js
@@ -0,0 +1,66 @@
+import { expect } from "chai";
+import path from "path";
+import { readFileSync } from "fs";
+
+import { loadScenarios } from "./utils/loadScenarios";
+import { dirname } from "./utils/dirname";
+
+const __dirname = dirname(import.meta.url);
+const { main: modulePath } = JSON.parse(
+  readFileSync(path.resolve(__dirname, "../package.json"))
+);
+
+// get the correct module value from package.json and test that
+const { mf2, __esModule } = await import(
+  path.resolve(__dirname, "../", modulePath)
+);
+
+const scenarioDir = path.resolve(
+  __dirname,
+  `../node_modules/microformat-tests/tests`
+);
+
+const v1 = loadScenarios(scenarioDir, "microformats-v1");
+const v2 = loadScenarios(scenarioDir, "microformats-v2");
+const mixed = loadScenarios(scenarioDir, "microformats-mixed");
+
+const options = {
+  baseUrl: "http://example.com",
+};
+
+describe("package // cjs // scenarios", () => {
+  it("should have a .cjs extension", () => {
+    expect(modulePath).to.match(/\.cjs$/);
+  });
+
+  it("should have __esModule = true", () => {
+    expect(__esModule).to.equal(true);
+  });
+
+  describe("microformats-v1", () => {
+    v1.forEach(({ name, input, expected }) => {
+      it(`should correctly parse ${name}`, () => {
+        const result = mf2(input, options);
+        expect(result).to.deep.equal(expected);
+      });
+    });
+  });
+
+  describe("microformats-v2", () => {
+    v2.forEach(({ name, input, expected }) => {
+      it(`should correctly parse ${name}`, () => {
+        const result = mf2(input, options);
+        expect(result).to.deep.equal(expected);
+      });
+    });
+  });
+
+  describe("microformats-mixed", () => {
+    mixed.forEach(({ name, input, expected }) => {
+      it(`should correctly parse ${name}`, () => {
+        const result = mf2(input, options);
+        expect(result).to.deep.equal(expected);
+      });
+    });
+  });
+});
