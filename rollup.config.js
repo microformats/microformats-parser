ec22ba781ac323f497f9f9f1458ea587761ad9eb
aimee-gm <12508200+aimee-gm@users.noreply.github.com>
2023-09-13 22:18:42 +0100

    chore(deps): downgrade rollup-plugin-dts

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import sourceMaps from "rollup-plugin-sourcemaps";
 import { terser } from "rollup-plugin-terser";
-import { dts } from "rollup-plugin-dts";
+import dts from "rollup-plugin-dts";
 
 export default [
   {
