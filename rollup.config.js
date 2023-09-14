a956df0a76c9660c4aac14608666cd574e7ef65b
aimee-gm <12508200+aimee-gm@users.noreply.github.com>
2023-09-14 08:36:35 +0100

    include metas in demo bundle

--- rollup.config.js
+++ rollup.config.js
@@ -90,6 +90,7 @@ export default [
             title,
             name,
             description,
+            metas,
             version: `v${version}`,
             repo,
             releases: `${repo}/releases`,
